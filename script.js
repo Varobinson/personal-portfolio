a1bfce3ccccbad3eb8a11b9f9bf3990634d7c798
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-12-07 17:40:32 -0500

    added resume and embedvideo

--- script.js
+++ script.js
@@ -10,38 +10,23 @@ window.onscroll = () => {
     if(this.scrollY <= 1) nav.className = ' navbar fixed-top navbar-expand-lg'; else nav.className = ' scroll navbar fixed-top navbar-expand-lg';
   };
 
-let project = document.querySelector('.bud')
-let project2 = document.querySelector('.bud2')
-let card = document.querySelector('.item')
-let card2 = document.querySelector('.item2')
-let project3 = document.querySelector('.bud3')
-let card3 = document.querySelector('.item3')
+let projects = document.querySelectorAll('.item')
+let menuItems = document.querySelectorAll('.nav-link')
 
-  project.addEventListener('mouseenter',function(){
-    document.querySelector(".dropZone").style.display = "block";
-    document.querySelector(".dropZone").style.transition = "all 0.5s !important";
-    document.querySelector(".dropZone").style.margin = "30px !important";
-    document.querySelector(".dropZone").style.transition = "transition: all 0.8s !important";
+projects.forEach(project =>{
+  project.addEventListener('mouseenter',function(e){
+    e.target.querySelector(".dropZone").classList.add('active')
+     
   })
-  card.addEventListener('mouseleave',function(){
-    document.querySelector(".dropZone").style.display = "none";
-    document.querySelector(".dropZone").style.transition = "all 0.8s !important";
+  project.addEventListener('mouseleave',function(e){
+    e.target.querySelector(".dropZone").classList.remove('active')
+     $('#navbarText').collapse('hide')
   })
-  project2.addEventListener('mouseenter',function(){
-    document.querySelector(".dropZone2").style.display = "block";
-    document.querySelector(".dropZone2").style.transition = "all 0.5s !important";
-    document.querySelector(".dropZone2").style.margin = "30px !important";
+
+})
+ 
+menuItems.forEach(item =>{
+  item.addEventListener('click', function(e){
+    
   })
-  card2.addEventListener('mouseleave',function(){
-    document.querySelector(".dropZone2").style.display = "none";
-    document.querySelector(".dropZone2").style.transition = "all 0.8s !important";
-  })
-  project3.addEventListener('mouseenter',function(){
-    document.querySelector(".dropZone3").style.display = "block";
-    document.querySelector(".dropZone3").style.transition = "all 0.5s !important";
-    document.querySelector(".dropZone3").style.margin = "30px !important";
-  })
-  card3.addEventListener('mouseleave',function(){
-    document.querySelector(".dropZone3").style.display = "none";
-    document.querySelector(".dropZone3").style.transition = "all 0.8s !important";
-  })
+})
