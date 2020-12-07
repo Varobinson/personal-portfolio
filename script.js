1f2af1c36a0225c98345e7ca976f8b486c12c4af
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-12-07 18:29:09 -0500

    fixed script

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ window.addEventListener('onload', function(){
 window.onscroll = () => {
     const nav = document.querySelector('.navbar');
     if(this.scrollY <= 1) nav.className = ' navbar fixed-top navbar-expand-lg'; else nav.className = ' scroll navbar fixed-top navbar-expand-lg';
+    
+    
   };
 
 let projects = document.querySelectorAll('.item')
 let menuItems = document.querySelectorAll('.nav-link')
 
-$(window).width() > 570 ?  projects.forEach(project =>{project.addEventListener('mouseenter',function(e){e.target.querySelector(".dropZone").classList.add('active')})
+$(window).width() > 570 ?  projects.forEach(project =>{
+  project.addEventListener('mouseenter',function(e){
+    e.target.querySelector(".dropZone").classList.add('active')
+     
+  })
   project.addEventListener('mouseleave',function(e){
     e.target.querySelector(".dropZone").classList.remove('active')
      $('#navbarText').collapse('hide')
   })
 
-})
+}): ''
  
 
