c010a14a7e8475d25e2c8bdf6ca5d2979227c493
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-08-11 13:32:52 -0400

    no video

--- video.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    let vid = document.querySelector('.playing')
-    // vid.play();
-    vid.addEventListener('ended',()=>{
-      window.location = 'http://127.0.0.1:5500/home.html'
-      
-    })
-  });
